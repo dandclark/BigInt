052d74b5060e6862eead244c351e2882aafca41c
Dan Clark <thedanclark@gmail.com>
2014-05-25 13:41:02 -0700

    Added demo.c containing basic usage examples.

---
+++ demo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#include "BigInt.h"
+
+// Demo of some basic BigInt functionality
+
+int main() {
+    // Obtain a BigInt initialized to 42
+    BigInt* new_big_int = BigInt_construct(42);
+    
+    // Get a normal int back (the BigInt must fit in an int type)
+    int as_int = BigInt_to_int(new_big_int);
+
+    // Print to stdout with BigInt_print 
+    printf("BigInt is: ");
+    BigInt_print(new_big_int);
+    printf("\nAs int: %i\n", as_int);
+
+    // The caller is responsible for freeing a BigInt allocated with
+    // BigInt_construct with a call to BigInt_free: 
+    BigInt_free(new_big_int);
+
+    // BigInt operations take two BigInt parameters and place the result in the first parameter:
+    BigInt* a = BigInt_construct(15);
+    BigInt* b = BigInt_construct(-20);
+    BigInt_add(a, b);
+    printf("Addition result: ");
+    BigInt_print(a); // Prints -5
+    printf("\n");
+
+    // The exception is BigInt_compare; this takes two BigInt parameters, changes neither, and returns the value of the comparison:
+    BigInt_assign_int(a, 15);
+    BigInt_assign_int(b, -20);
+    printf("Comparison results:\n");
+    printf("%i\n", BigInt_compare(a, b)); // prints 1
+    printf("%i\n", BigInt_compare(b, a)); // prints -1
+    printf("%i\n", BigInt_compare(a, a)); // prints 0
+
+    BigInt_free(a); 
+    BigInt_free(b); 
+
+    return 0;
+}
+
+
+
--- test.c
+++ test.c
@@ -3,27 +3,11 @@
 
 #include "BigInt.h"
 
+// Run BigInt unit tests
+
 int main() {
 
-    printf("Hello, world!\n");
-    printf("Size: %i\n", sizeof(struct BigInt));
-
-
-    BigInt* big_int;
-    big_int = BigInt_construct(42);
-
-    BigInt_add(big_int, big_int);
-
-    printf("Number is: ");
-    BigInt_print(big_int);
-    printf("\n");
-
-    int as_int = BigInt_to_int(big_int);
-    printf("Number as int: %i\n", as_int);
-
     BigInt_test_basic();
-
-    BigInt_free(big_int); 
 
     return 0;
 }
